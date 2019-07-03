aa0049c28477e4adfd152f014c85df6b9de9ec10
Jae Lee <jael@stanford.edu>
2019-07-03 15:00:27 -0700

    Copy over ssn_url to admin_data form

--- SSNMasker.php
+++ SSNMasker.php
@@ -8,6 +8,72 @@ use REDCap;
 class SSNMasker extends \ExternalModules\AbstractExternalModule {
 
 
+public function redcap_save_record($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash = NULL, $response_id = NULL, $repeat_instance) {
+    $target_form = $this->getProjectSetting('ssn-form');
+
+    //$this->emDebug($target_form . " vs " . $instrument);
+
+    if ($instrument == $target_form) {
+        //if the ssn field has been entered, then populate the ssn field on the admin data form.
+        $ssn_field = $this->getProjectSetting('ssn-field');
+        $ssn_url_field = $this->getProjectSetting('ssn-url-field');
+
+        //if either are empty, bail
+        if (empty($ssn_field) || empty($ssn_url_field)) {
+            $this->emError("SSN field and SSN URL field not set.  Unable to set ssn url for this project ($project_id).");
+            $this->exitAfterHook();
+        }
+
+        $results = $this->getFieldValues($record, $event_id, array($ssn_field, $ssn_url_field));
+        $ssn_value = current($results)[$ssn_field];
+        //$this->emDebug("retrieving for record $record: ", $ssn_field . ": " . $ssn_value, $ssn_url_field, $results);
+
+        if (!empty($ssn_value)) {
+            //if ssn has been set, then construct the ssn_url and save it to the admin data form
+            $api_url = $this->getUrl('src/Viewer.php',true,true);
+            $ssn_url = $api_url."&record=".$record;
+
+            $data = array(
+                REDCap::getRecordIdField() => $record,
+                'redcap_event_name'        => REDCap::getEventNames(true,false, $event_id),
+                $ssn_url_field             => $ssn_url
+            );
+
+            $response = REDCap::saveData('json', json_encode(array($data)));
+
+            if (!empty($response['errors'])) {
+                $msg = "Error saving SSN URL field to admin_data form";
+                $this->emError($msg, $response['errors'], $data);
+
+                REDCap::logEvent(
+                    $msg,
+                    "Unable to set the SSN Url field for this record: ".$response['errors'],
+                    NULL,
+                    $record
+                );
+
+            }
+
+        }
+
+    }
+}
+
+function getFieldValues($record, $event_id, $target_fields) {
+    $params = array(
+        'return_format' => 'json',
+        'records' => $record,
+        'fields' => $target_fields,
+        'events' => $event_id
+    );
+
+    $q = REDCap::getData($params);
+    $results = json_decode($q, true);
+
+    //$this->emDebug($results);
+    return $results;
+}
+
     function checkIfAuthorizedUser($sunet_id,  $approved_users, $approved_users_2) {
 
         $group = $other_group = null;
